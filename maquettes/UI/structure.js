82f6b2c6f2e8b2488ca2ae1e864994f5cffc3dc9
Mathieu Lavigne <mathieu.lavigne@capgemini.com>
2018-12-17 22:54:08 +0100

    Anciennes modifs

---
+++ maquettes/UI/structure.js
@@ -0,0 +1,10 @@
+{
+  version: 1,
+  date: '2015-12-17',
+  sections: [
+    {
+      pattern: {},
+      measures: [{}, {}, {}, {}]
+    }
+  ]
+}
---
+++ typings/tsd.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="async/async.d.ts" />
+/// <reference path="node/node.d.ts" />
+/// <reference path="underscore/underscore.d.ts" />
+/// <reference path="xml2js/xml2js.d.ts" />
